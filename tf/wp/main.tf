182a0373545103a70f919822027e11f96132eca9
Vitaly Belousov <vitaly.belousov@gmail.com>
2019-08-06 00:18:29 -0700

    added vpc and launch config

--- tf/wp/main.tf
+++ tf/wp/main.tf
@@ -1,2 +1,18 @@
 data "aws_caller_identity" "current" {
 }
+
+module "vpc" {
+	source = "./modules/vpc"
+    env = "${var.env}"
+    app = "${var.app}"
+
+}
+
+module "asg" {
+    source = "./modules/asg"
+    region = "${var.region}"
+    env = "${var.env}"
+    app = "${var.app}"
+    asg_min_size = "1"
+    asg_max_size = "1"
+}
