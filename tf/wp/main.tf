14850c244b459296ab362e6e17ffdb7a03197365
Vitaly Belousov <vitaly.belousov@gmail.com>
2019-08-06 01:22:04 -0700

    added asg, vpc and alb

--- tf/wp/main.tf
+++ tf/wp/main.tf
@@ -5,7 +5,20 @@ module "vpc" {
 	source = "./modules/vpc"
     env = "${var.env}"
     app = "${var.app}"
+}
 
+module "iam" {
+	source = "./modules/iam"
+    env = "${var.env}"
+    app = "${var.app}"
+}
+
+module "alb" {
+	source = "./modules/alb"
+    env = "${var.env}"
+    app = "${var.app}"
+    vpc_id = "${module.vpc.vpc_id}"
+    public_subnets = "${module.vpc.public_subnets}"
 }
 
 module "asg" {
@@ -15,4 +28,7 @@ module "asg" {
     app = "${var.app}"
     asg_min_size = "1"
     asg_max_size = "1"
+    instance_profile = "${module.iam.instance_profile}"
+    vpc_id = "${module.vpc.vpc_id}"
+    alb_sg_id = "${module.alb.alb_sg_id}"
 }
